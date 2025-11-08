24e8c41a6f686e2bd260157a1d41ec294d796c39
Long Tran <longtran13112004@gmail.com>
2025-11-08 21:13:45 +0700

    Initial commit

---
+++ untitled/src/ss_1/baitap/Demo.java
@@ -0,0 +1,14 @@
+package ss_1.baitap;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        String name = "Tran Van Long";
+        System.out.println("Toi ten la: " + name);
+        System.out.println("So nhap vao la: " + number);
+        System.out.println("Nhap vao ");
+    }
+}
---
+++ untitled/src/ss_1/thuchanh/SystemTime.java
@@ -0,0 +1,10 @@
+package ss_1.thuchanh;
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Thoi gian thuc: " +  now);
+    }
+}
