c3da86a01c4a6e826b926a813c4e632b22cc4070
Long Tran <longtran13112004@gmail.com>
2025-11-10 19:49:53 +0700

    add file SystemTimee.java

--- untitled/src/ss_1/baitap/SystemTimee.java
+++ untitled/src/ss_1/baitap/SystemTimee.java
@@ -1,8 +1,8 @@
-package ss_1.thuchanh;
+package ss_1.baitap;
 
 import java.util.Date;
 
-public class SystemTime {
+public class SystemTimee {
     public static void main(String[] args) {
         Date now = new Date();
         System.out.println("Thoi gian thuc: " +  now);
